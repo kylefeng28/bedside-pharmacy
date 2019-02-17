3111caea2e423afa1cd3cbf02237a8844e3a19db
Kyle Feng <kylefeng28@gmail.com>
2019-02-17 13:08:36 -0500

    Display data correctly in grid

--- data-editor/src/Editor.js
+++ data-editor/src/Editor.js
@@ -8,10 +8,46 @@ import 'react-table/react-table.css';
 // import * as R from 'ramda';
 
 import {benzodiazepines} from './mock_data';
+import { access } from "fs";
 const R = require('ramda');
 
 function makeData() {
   return R.flatten(R.repeat(benzodiazepines.drugs, 20));
+}
+
+const EmptyRowsView = () => {
+  const message = "No data to show (yet!)";
+  return (
+    <div style={{ textAlign: "center", backgroundColor: "#ddd", padding: "100px" }}>
+      {/* <img src="./logo.png" alt={message} /> */}
+      <h3>{message}</h3>
+    </div>
+  );
+};
+
+const SubcellFormatter = ({ subfield, subvalue }) => {
+  return (
+    <div>
+      <b>{subfield}:</b><br/>
+      {subvalue}
+    </div>
+  )
+}
+
+const CellFormatter = (props) => {
+  // value contains the original (JSON) data of the cell
+  const { value } = props;
+  const subcells = Object.entries(value).map((entry) => {
+    let [ subfield, subvalue ] = entry; // destructure entry
+    return (<SubcellFormatter subfield={subfield} subvalue={subvalue} />);
+  });
+
+  // TODO align left
+  return (
+    <div style={{textAlign: 'left', verticalAlign: 'top'}}>
+      {subcells}
+    </div>
+  );
 }
 
 export default class Editor extends React.Component {
@@ -21,9 +57,11 @@ export default class Editor extends React.Component {
       data: makeData()
     };
   }
+
   render() {
     const { data } = this.state;
-    console.log(data)
+
+    const accessData = (key) => (d) => d.data[key]
 
     return (
       <div>
@@ -31,11 +69,22 @@ export default class Editor extends React.Component {
           data={data}
           // id: 'dose', // only needed if accessor is not a string
           columns={[
-            { Header: "DOSE", id: 'a', accessor: d => JSON.stringify(d.data["DOSE"]) },
-            { Header: "ONSET/DURATION", id: 'b', accessor: d => JSON.stringify(d.data["DOSE"]) },
-            { Header: "ONSET/DURATION", id: 'c', accessor: d => JSON.stringify(d.data["ONSET/DURATION"]) },
-            { Header: "METABOLISM/EXCRETION", id: 'd', accessor: d => JSON.stringify(d.data["METABOLISM/EXCRETION"]) },
-            { Header: "WARNINGS", id: 'e', accessor: d => JSON.stringify(d.data["WARNINGS"]) }
+            { Header: "Name", id: 'name', accessor: "name" },
+            { Header: "DOSE", id: 'dose',
+              accessor: accessData("DOSE"), Cell: CellFormatter
+            },
+            {
+              Header: "ONSET/DURATION", id: 'onset_duration',
+              accessor: accessData("ONSET/DURATION"), Cell: CellFormatter
+            },
+            {
+              Header: "METABOLISM/EXCRETION", id: 'metabolism_excretion',
+              accessor: accessData("METABOLISM/EXCRETION"), Cell: CellFormatter
+            },
+            {
+              Header: "WARNINGS", id: 'warnings',
+              accessor: accessData("WARNINGS"), Cell: CellFormatter
+            }
           ]}
           defaultPageSize={30}
           className="-striped -highlight"

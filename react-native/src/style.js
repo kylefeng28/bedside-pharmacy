fbf76e3717cc00bafb133bb0ca7d53c587a314f0
yujie-tao <yujietao@outlook.com>
2019-02-23 10:30:13 -0500

    switch to external stylesheet

---
+++ react-native/src/style.js
@@ -0,0 +1,34 @@
+import React, {StyleSheet} from 'react-native'
+
+export default StyleSheet.create({
+
+ app: {
+    marginHorizontal: 'auto',
+    maxWidth: 500
+  },
+  logo: {
+    height: 80
+  },
+  header: {
+    padding: 20
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 15,
+    marginVertical: 10,
+    textAlign: 'center'
+  },
+  text: {
+    lineHeight: 15,
+    fontSize: 11.25,
+    marginVertical: 10,
+    textAlign: 'center'
+  },
+  link: {
+    color: '#1B95E0'
+  },
+  code: {
+    // fontFamily: 'monospace, monospace'
+  }
+  
+});

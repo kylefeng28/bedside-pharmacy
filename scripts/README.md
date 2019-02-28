c6f08763e0b85abc439b49ce317261317698b833
Kyle Feng <kylefeng28@gmail.com>
2019-02-28 17:26:01 -0500

    Update firebase_tools script

---
+++ scripts/README.md
@@ -0,0 +1,14 @@
+# Scripts
+
+Install dependencies with pip first:
+
+```
+pip3 install -r requirements.txt
+```
+
+## `parse_excel.py`
+Takes in an Excel .xlsx file and converts it (hopefully) into a .json file.
+
+## `firebase_tools.py`
+Place a `environment.json` with the appropriate Firebase information in this directory.
+Login with your credentials.
--- scripts/firebase_tools.py
+++ scripts/firebase_tools.py
@@ -1,6 +1,7 @@
 import pyrebase
 from PyInquirer import prompt
 import json
+import time
 
 def initialize(env):
     config = {
@@ -29,6 +30,30 @@ def prompt_login():
     answers = prompt(questions)
     return answers['email'], answers['password']
 
+def prompt_action():
+    questions = [
+        {
+            'type': 'list',
+            'name': 'action',
+            'message': 'What do you want to do?',
+            'choices': [
+                { 'name': 'Export database', 'value': 'export'  },
+                { 'name': 'Listen for changes', 'value': 'listen'  },
+                #  { 'name': 'Import JSON to database', 'value': 'import'  },
+                { 'name': 'Quit', 'value': 'quit'  },
+            ]
+        },
+        # {
+        #     'type': 'confirm',
+        #     'name': 'toBeDelivered',
+        #     'message': 'Is this for delivery?',
+        #     'default': False
+	# }
+    ]
+    print('Please select an action:')
+    answers = prompt(questions)
+    return answers['action']
+
 if __name__ == '__main__':
     # Read from environment file
     with open('environment.json') as f:
@@ -42,9 +67,40 @@ if __name__ == '__main__':
     email, password = prompt_login()
     user = auth.sign_in_with_email_and_password(email, password)
 
+    # TODO have an option to use idToken
+    #  with open('.env') as f:
+    #      user = {}
+    #      user['idToken'] = f.read()
+
+    print('Using token: ' + user['idToken'])
+
     # Get a reference to the database
     db = firebase.database()
 
-    # Print out everything in JSON format
-    ref = db.child('').get(user['idToken'])
-    print(json.dumps(ref.val()))
+    # Loop forever
+    while True:
+        action = prompt_action()
+        if action == 'export':
+            # Export out everything in JSON format
+            fname = 'output.json'
+            ref = db.child('').get(user['idToken'])
+            val = ref.val()
+            with open(fname, 'w') as f:
+                json.dump(val, f, indent=4)
+                print('Exported to ' + fname)
+
+        elif action == 'listen':
+            def stream_handler(msg):
+                t = time.strftime('%d %b %Y %H:%M:%S', time.localtime())
+                print('[%s] \x1b[33m%s %s\x1b[0m' % (t, msg['event'], msg['path']))
+
+            stream = db.child('').stream(stream_handler)
+            input('Now listening for changes. Press enter to stop listening...\n')
+            stream.close()
+
+        # elif action == 'import':
+        #     pass
+
+        elif action == 'quit':
+            print('Goodbye!')
+            exit(0)

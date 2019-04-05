ad6eed8ba493b0b84a44989cab4c1d29f1a442ad
he135 <thereseym@gmail.com>
2019-04-04 22:11:31 -0400

    added compare page

--- react-native/src/screens/Compare.tsx
+++ react-native/src/screens/Compare.tsx
@@ -6,12 +6,17 @@ import {
   ScrollView,
   StyleSheet,
   TouchableOpacity,
+  Picker
 } from 'react-native';
 
 import React, { Component } from 'react';
 import { Container, Header, Left, Body, Right, Button, H1, H2, H3, Title, Card, CardItem, Content, FooterTab, Icon, Footer, List, ListItem } from 'native-base';
-import { Ionicons, MaterialCommunityIcons, Entypo } from '@expo/vector-icons';
+import { Ionicons, MaterialCommunityIcons, Entypo, SimpleLineIcons} from '@expo/vector-icons';
 
+import styles from '../style';
+
+// let colors = ['#d9b3ff', '#b3e0ff'];
+let colors = ['#ffffff', '#f2f2f2']
 
 export class Compare extends Component {
 
@@ -35,10 +40,79 @@ export class Compare extends Component {
   render() {
     return (
        <Container>
-         <Content padder>
-           <Text>hello</Text>
+          <Content padder>
+            <View style={{paddingLeft: 15, paddingRight: 15}}>
+
+              <View style={styles.compare_title_container}>
+                <Text style={styles.compare_title}>Compare</Text>
+                <SimpleLineIcons name="plus" style={[styles.add_icon, {paddingRight: 5}]}></SimpleLineIcons>
+              </View>
+
+              <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                <View style={{flex:.5}}>
+                  <Text>Compare by</Text>
+                </View>
+                <View style={{flex:.5}}>
+                  <Picker
+                    mode = "dropdown"
+                    selectedValue={this.state.language}
+                    // style={{height: 50, width: 100}}
+                    onValueChange={(itemValue, itemIndex) =>
+                      this.setState({language: itemValue})
+                    }>
+                    <Picker.Item label="Onset/Duration" value="onset"/>
+                    <Picker.Item label="Dose" value="dose"/>
+                    <Picker.Item label="Metabolism/Excretion" value="metabolism"/>
+                  </Picker>
+                </View>
+              </View>
+
+            </View>
+
+            <View style={styles.class_list}>
+              <FlatList
+                // hardcoded example data:
+                data={[{name: 'Lorazepam', onset: '15-20 minutes', duration: '8 to 15 hours', metabolism: 'Hepatic'},
+                  {name: 'Midazolam', onset: '15 minutes', duration: '<2 hours', metabolism: 'Urine metabolites'}]}
+                // tslint:disable-next-line:jsx-no-lambda
+                renderItem={({item, index}) => 
+                  <View style={{ backgroundColor: colors[index % colors.length] }}>
+                    <ListItem noIndent>
+                      <View style={styles.container}>
+
+                        <View style={styles.column_header}>
+                          <Text style={styles.column_header_text}>{item.name}</Text>
+                        </View>
+                        
+                        <View style={styles.item}>
+                          <Text style={{fontWeight:'bold'}}>Onset:</Text>
+                        </View>
+                        <View style={styles.item}><Text>{item.onset}</Text></View>
+
+                        <View style={styles.item}>
+                          <Text style={{fontWeight:'bold'}}>Duration:</Text>
+                        </View>
+                        <View style={styles.item}><Text>{item.duration}</Text></View>
+
+                        <View style={styles.item}>
+                          <Text style={{fontWeight:'bold'}}>Metabolism</Text>
+                        </View>
+                        <View style={styles.item}><Text>{item.metabolism}</Text></View>
+
+                      </View>
+
+                      <Ionicons name="ios-close" style={[styles.close_icon, {paddingRight: 15}]}></Ionicons>
+                    </ListItem>
+                  </View>
+                }
+              />
+            </View>
+
           </Content>
       </Container>
     );
   }
 }
+
+// TODO: CONNECT TO ACTUAL BACKEND
+// Yujie will fix font styles
--- react-native/src/style.js
+++ react-native/src/style.js
@@ -1,227 +1,278 @@
-import React, {StyleSheet} from 'react-native'
+import React, { StyleSheet } from 'react-native'
 import EStyleSheet from 'react-native-extended-stylesheet';
 
-EStyleSheet.build({ 
-  
-  /* ==========================================================================
-   Global styles
-   ========================================================================== */
+EStyleSheet.build({
 
-  // Global colors
-  $CarolinaBlue: '#4B9CD3',
-  $LinkBlue: '#007FAE',
-  $AthleticsNavy: '#13294B',
-  $DarkGray: '#767676',
-  $MediumGray: '#E1E1E1',
-  $LightGray: '#FAFAFA',
-  $MainBlack: '#151515',
-  $MainWhite: '#FFFFFF'
+    /* ==========================================================================
+     Global styles
+     ========================================================================== */
+
+    // Global colors
+    $CarolinaBlue: '#4B9CD3',
+    $LinkBlue: '#007FAE',
+    $AthleticsNavy: '#13294B',
+    $DarkGray: '#767676',
+    $MediumGray: '#E1E1E1',
+    $LightGray: '#FAFAFA',
+    $MainBlack: '#151515',
+    $MainWhite: '#FFFFFF'
 });
 
 export default EStyleSheet.create({
-   /* ==========================================================================
-   Global styles
-   ========================================================================== */
-  body:{
-    marginLeft: '2%',
-    marginRight: '2%'
-  },
+    /* ==========================================================================
+    Global styles
+    ========================================================================== */
+    body: {
+        marginLeft: '2%',
+        marginRight: '2%'
+    },
 
-  header:{
-    marginBottom: 0,
-    marginBottom: 15,
-    backgroundColor: '$LightGray',
-    padding: '4%',
-  },
+    header: {
+        marginBottom: 0,
+        marginBottom: 15,
+        backgroundColor: '$LightGray',
+        padding: '4%',
+    },
 
-  header_text_box:{
-    flex: 2,
-  },
+    header_text_box: {
+        flex: 2,
+    },
 
-  header_icon:{
-    fontSize:32,
-    color: '$DarkGray'
-  },
+    header_icon: {
+        fontSize: 32,
+        color: '$DarkGray'
+    },
 
-  header_text:{
-    fontSize: 18,
-    color: '$MainBlack',
-    fontFamily:'Open-Sans-SemiBold'
-  },
+    header_text: {
+        fontSize: 18,
+        color: '$MainBlack',
+        fontFamily: 'Open-Sans-SemiBold'
+    },
 
-  inline:{
-    flex: 1, 
-    flexDirection: 'row'
-  },
+    inline: {
+        flex: 1,
+        flexDirection: 'row'
+    },
 
-  drug_name:{
-    fontSize: 30,
-    fontFamily: 'Open-Sans-Bold',
-    color: '$MainBlack'
-  },
+    drug_name: {
+        fontSize: 30,
+        fontFamily: 'Open-Sans-Bold',
+        color: '$MainBlack'
+    },
 
-  add_comparison:{
-    fontSize: 32,
-    color: '$DarkGray',
-  },
+    add_comparison: {
+        fontSize: 32,
+        color: '$DarkGray',
+    },
 
-  footer:{
-    backgroundColor: '$LightGray'
-  },
+    footer: {
+        backgroundColor: '$LightGray'
+    },
 
-  footer_icon:{
-    fontSize: 32,
-    // color: '$LinkBlue'
-  },
+    footer_icon: {
+        fontSize: 32,
+        // color: '$LinkBlue'
+    },
 
-   /* ==========================================================================
-   Home Page
-   ========================================================================== */
+    /* ==========================================================================
+    Home Page
+    ========================================================================== */
 
-   title:{
-     fontSize: 30,
-     fontFamily: 'Open-Sans-Bold',
-     marginTop: 30,
-     marginBottom: 10,
-     color: '$MainBlack'
-   },
+    title: {
+        fontSize: 30,
+        fontFamily: 'Open-Sans-Bold',
+        marginTop: 30,
+        marginBottom: 10,
+        color: '$MainBlack'
+    },
 
-   title_light:{
-     fontFamily: 'Open-Sans-Light'
-   },
+    title_light: {
+        fontFamily: 'Open-Sans-Light'
+    },
 
-   by_class:{
-     fontSize: 20,
-     fontFamily: 'Open-Sans-Bold',
-     color: '$MainBlack'
-   },
+    by_class: {
+        fontSize: 20,
+        fontFamily: 'Open-Sans-Bold',
+        color: '$MainBlack'
+    },
 
-   search_icon:{
-     color: '$DarkGray',
-     fontSize: 30
-   },
+    search_icon: {
+        color: '$DarkGray',
+        fontSize: 30
+    },
 
-   class_list:{
-     marginTop: 30
-   },
+    class_list: {
+        marginTop: 30
+    },
 
-   class_list_item:{
-     fontSize: 18,
-     fontFamily: 'Open-Sans-Regular',
-     color:'$LinkBlue'
-   },
+    class_list_item: {
+        fontSize: 18,
+        fontFamily: 'Open-Sans-Regular',
+        color: '$LinkBlue'
+    },
 
-   forward_icon:{
-     fontSize: 20,
-     color: '$MediumGray'
-   },
+    forward_icon: {
+        fontSize: 20,
+        color: '$MediumGray'
+    },
 
-  /* ==========================================================================
-   Subclass Page
-   ========================================================================== */
+    /* ==========================================================================
+     Subclass Page
+     ========================================================================== */
 
-   circle_icon:{
-     fontSize: 18,
-     color:'$LinkBlue',
-   },
+    circle_icon: {
+        fontSize: 18,
+        color: '$LinkBlue',
+    },
 
-   subclass_list_item:{
-     height: 60
-   },
+    subclass_list_item: {
+        height: 60
+    },
 
-   subclass_list_item_text:{
-     fontSize: 18,
-     fontFamily: 'Open-Sans-Regular',
-     color:'$MainBlack',
-     marginLeft: 8,
-   },
+    subclass_list_item_text: {
+        fontSize: 18,
+        fontFamily: 'Open-Sans-Regular',
+        color: '$MainBlack',
+        marginLeft: 8,
+    },
 
-   /* ==========================================================================
-   Druglist Page
-   ========================================================================== */
+    /* ==========================================================================
+    Druglist Page
+    ========================================================================== */
 
-   add_icon:{
-     fontSize: 30,
-     color: '$LinkBlue'
-   },
+    add_icon: {
+        fontSize: 30,
+        color: '$LinkBlue'
+    },
 
-   drug_list_item:{
-     height: 60
-   },
+    drug_list_item: {
+        height: 60
+    },
 
-   drug_list_item_text:{
-     fontSize: 18,
-     fontFamily: 'Open-Sans-Regular',
-     color:'$MainBlack',
-     marginLeft: 8,
-   },
+    drug_list_item_text: {
+        fontSize: 18,
+        fontFamily: 'Open-Sans-Regular',
+        color: '$MainBlack',
+        marginLeft: 8,
+    },
 
-  /* ==========================================================================
-   DrugInfo Page
-   ========================================================================== */
+    /* ==========================================================================
+     DrugInfo Page
+     ========================================================================== */
 
-  brand_name:{
-    fontSize: 16,
-    fontFamily: 'Open-Sans-Bold',
-    color: '$MainBlack',
-  },
+    brand_name: {
+        fontSize: 16,
+        fontFamily: 'Open-Sans-Bold',
+        color: '$MainBlack',
+    },
 
-  subclass_name:{
-    fontSize: 14,
-    fontFamily: 'Open-Sans-Italic',
-    width: '95%',
-    color: '$DarkGray',
-    marginBottom: 20
-  },
+    subclass_name: {
+        fontSize: 14,
+        fontFamily: 'Open-Sans-Italic',
+        width: '95%',
+        color: '$DarkGray',
+        marginBottom: 20
+    },
 
-  indication_header: {
-    padding: 25,
-    borderRadius: 4,
-    borderWidth: 0.5,
-    borderColor: '$MediumGray',
-    alignItems:'flex-start',
-  },
+    indication_header: {
+        padding: 25,
+        borderRadius: 4,
+        borderWidth: 0.5,
+        borderColor: '$MediumGray',
+        alignItems: 'flex-start',
+    },
 
-  indication_headerText: {
-    fontSize: 18,
-    textAlign: 'left',
-    color: '$LinkBlue',
-  },
+    indication_headerText: {
+        fontSize: 18,
+        textAlign: 'left',
+        color: '$LinkBlue',
+    },
 
-  indication_content: {
-    fontSize: 16,
-    padding: 20,
-    backgroundColor: '#fff',
-    
-    borderRadius: 4,
-    borderWidth: 0.5,
-    borderColor: '$MediumGray',
-    alignItems:'center',
-  },
+    indication_content: {
+        fontSize: 16,
+        padding: 20,
+        backgroundColor: '#fff',
 
-  indication_active: {
-    backgroundColor: '$LinkBlue',
-    color: '$MainWhite'
-  },
+        borderRadius: 4,
+        borderWidth: 0.5,
+        borderColor: '$MediumGray',
+        alignItems: 'center',
+    },
 
-  indication_inactive: {
-    backgroundColor: '$MainWhite',
-  },
+    indication_active: {
+        backgroundColor: '$LinkBlue',
+        color: '$MainWhite'
+    },
 
-  indication_header_active:{
-    color: '$MainWhite',
-    backgroundColor: '$LinkBlue'
-  },
+    indication_inactive: {
+        backgroundColor: '$MainWhite',
+    },
 
-  indication_header_inactive:{
-    color: '$LinkBlue'
-  },
+    indication_header_active: {
+        color: '$MainWhite',
+        backgroundColor: '$LinkBlue'
+    },
+
+    indication_header_inactive: {
+        color: '$LinkBlue'
+    },
 
 
 
-  /* ==========================================================================
-   Compare Page
-   ========================================================================== */
+    /* ==========================================================================
+     Compare Page
+     ========================================================================== */
+    compare_title_container: {
+        // width: '80%',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignItems: 'flex-start',
+        justifyContent: 'space-between',
+        marginTop: 30,
+        marginBottom: 10,
+    },
+
+    compare_title: {
+        fontSize: 30,
+        fontFamily: 'Open-Sans-Bold',
+        color: '$MainBlack'
+    },
+
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignItems: 'flex-start', // if you want to fill rows left to right
+        // width: '80%'
+    },
+
+    item: {
+        paddingTop: 2,
+        paddingBottom: 2,
+        width: '40%',
+        // width: '50%'
+    },
+
+    column_header: {
+        paddingTop: 2,
+        paddingBottom: 2,
+        width: '100%'
+    },
+
+    column_header_text: {
+        color: '$LinkBlue',
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+
+    close_icon: {
+        paddingRight: 5,
+        fontSize: 45,
+        // color: '$MediumGray',
+        color: '$DarkGray',
+        opacity: .5
+    },
+
+    // use add_icon from druglist page
 
 });
-

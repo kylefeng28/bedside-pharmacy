d7dbb8c27fc6a2487f765a0dd70cef971372ba5d
yujie-tao <yujietao@outlook.com>
2019-04-07 12:29:54 -0400

    add style to regsiter page

--- react-native/src/screens/UserSignup.tsx
+++ react-native/src/screens/UserSignup.tsx
@@ -1,13 +1,15 @@
 import {
   StyleSheet,
   Text,
+  View,
 } from 'react-native';
 
 import React, { Component } from 'react';
-import { Container, Header, Body, Form, Button, Item, Input, Picker, Title, Card, Content } from 'native-base';
+import { Container, Header, Body, Form, Button, Item, Input, Picker, Title, Label, Card, Content } from 'native-base';
 import { firebase } from '../utils/FirebaseWrapper';
 import styles from '../style';
 import { FirebaseError } from 'firebase';
+import RNPickerSelect from 'react-native-picker-select';
 
 const SIGNUP_INITIAL_STATE = {
   error: null,
@@ -72,68 +74,69 @@ export class UserSignup extends Component {
 
     return (
       <Container>
-        <Header style={styles.header}>
-          <Body style={styles.header_text_box}>
-            <Title style={styles.header_text}>Sign up</Title>
-          </Body>
-        </Header>
 
-        <Content padder style={styles.body}>
-          <Text>Please register</Text>
+        <Content padder>
+          <Text style={[styles.title,styles.main_title,styles.register_title]}>Create your account</Text>
+          <Text style={styles.register_subtitle}>Already has an account? <Text style={styles.switch_login}>Log in here</Text></Text>
 
-          {/* no onSubmit in native-base */}
-          <Form>
-            <Item>
-              <Input name="email"
-                placeholder="Email"
-                onChangeText={this.handleChangeGen('email')} />
+          <View style={styles.register_conainer}>
+            <Item stackedLabel>
+              <Label style={styles.label}>Email</Label>
+                <Input name="email"
+                  onChangeText={this.handleChangeGen('email')} />
             </Item>
-            <Item>
+
+            <Item stackedLabel>
+              <Label style={styles.label}>Password</Label>
               <Input name="passwordOne"
-                placeholder="Password"
                 onChangeText={this.handleChangeGen('passwordOne')} />
             </Item>
-            <Item>
+
+            <Item stackedLabel>
+              <Label style={styles.label}>Confirm Password</Label>
               <Input name="passwordTwo"
-                placeholder="Confirm password"
                 onChangeText={this.handleChangeGen('passwordTwo')} />
             </Item>
 
-            <Item>
-              <Picker name="gender"
-                mode="dropdown"
-                placeholder="I identify as ..."
-                selectedValue={this.state.gender}
-                onValueChange={this.handleChangeGen('gender')}>
-                <Picker.Item label="Male" value="male" />
-                <Picker.Item label="Female" value="female" />
-                <Picker.Item label="Other" value="other" />
-              </Picker>
-            </Item>
-
-            <Item>
-              <Picker name="profession"
-                mode="dropdown"
-                placeholder="I am a ..."
-                selectedValue={this.state.profession}
-                onValueChange={this.handleChangeGen('profession')}>
-                <Picker.Item label="Student" value="student" />
-                <Picker.Item label="Professional" value="professional" />
-              </Picker>
-            </Item>
-
-            <Item>
+            <Item stackedLabel>
+              <Label style={styles.label}>School</Label>
               <Input name="school"
-                placeholder="Where do/did you go to school?"
                 onChangeText={this.handleChangeGen('school')} />
             </Item>
 
+            <View style={styles.inline}>
+              <Item stackedLabel style={styles.half}>
+                <Label style={styles.label}>Gender</Label>
+                <Picker name="gender"
+                  mode="dropdown"
+                  selectedValue={this.state.gender}
+                  onValueChange={this.handleChangeGen('gender')}>
+                  <Picker.Item label="Male" value="male" />
+                  <Picker.Item label="Female" value="female" />
+                  <Picker.Item label="Other" value="other" />
+                </Picker>
+              </Item>
+
+              <Item stackedLabel style={styles.half}>
+              <Label style={styles.label}>Profession</Label>
+                <Picker name="profession"
+                  mode="dropdown"
+                  selectedValue={this.state.profession}
+                  onValueChange={this.handleChangeGen('profession')}>
+                  <Picker.Item label="Student" value="student" />
+                  <Picker.Item label="Professional" value="professional" />
+                </Picker>
+              </Item>
+            </View>
+
+  
             <Button block full
               onPress={this.handleSubmit}
-              disabled={isInvalid}>
-              <Text>Sign up</Text>
+              disabled={isInvalid}
+              style={styles.login_button}>
+              <Text style={styles.login_text}>Sign up</Text>
             </Button>
-          </Form>
+          </View>
 
         </Content>
       </Container>);

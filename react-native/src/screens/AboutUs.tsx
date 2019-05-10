159d06ab95c640467a84486df31b9c6605cfb0bf
yujie-tao <yujietao@outlook.com>
2019-05-09 21:32:58 -0700

    initialize about page

---
+++ react-native/src/screens/AboutUs.tsx
@@ -0,0 +1,20 @@
+import {
+  StyleSheet,
+  Text,
+} from 'react-native';
+
+import React, { Component } from 'react';
+import { Container, Header, Body, Form, Button, Item, Input, Title, Card, Content } from 'native-base';
+import { firebase } from '../utils/FirebaseWrapper';
+import styles from '../style';
+
+export class AboutUs extends Component {
+
+  render() {
+
+    return (
+      <Container>
+
+      </Container>);
+  }
+}
